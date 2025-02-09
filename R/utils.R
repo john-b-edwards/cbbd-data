494b35ab272b412292f157d3403b77f0739cac14
John Edwards <edwards1860@gmail.com>
2025-02-09 12:51:44 -0800

    add cbbd_save

--- R/utils.R
+++ R/utils.R
@@ -21,3 +21,27 @@ query_cbbd <- function(my_path, my_query) {
     RcppSimdJson::fparse() |>
     janitor::clean_names()
 }
+
+cbbd_save <- function(df, file_name, file_tag) {
+  temp_dir <- tempdir(check = TRUE)
+  saveRDS(df, file.path(temp_dir, paste0(file_name, ".rds")))
+  data.table::fwrite(df, file.path(temp_dir, paste0(file_name, ".csv")))
+  data.table::fwrite(df, file.path(temp_dir, paste0(file_name, ".csv.gz")))
+  arrow::write_parquet(df, file.path(temp_dir, paste0(file_name, ".parquet")))
+  qs::qsave(
+    df,
+    file.path(temp_dir, paste0(file_name, ".qs")),
+    preset = "custom",
+    algorithm = "zstd_stream",
+    compress_level = 22,
+    shuffle_control = 15
+  )
+  for (file in file.path(temp_dir)) {
+    piggyback::pb_upload(
+      file,
+      repo = "john-b-edwards/cbbd-data",
+      tag = file_tag,
+      .token = gh::gh_token()
+    )
+  }
+}
