3426d00ba7d07fde7610347a37e9e6b5714f6fcb
John Edwards <edwards1860@gmail.com>
2025-04-06 17:47:07 -0700

    add media

---
+++ src/update-media.R
@@ -0,0 +1,21 @@
+source("R/utils.R")
+
+build_media <- function(query_season = most_recent_season()) {
+  media <- query_games("games/media", query_season)
+  media <- media |>
+    tidyr::unnest_longer(broadcasts) |>
+    tidyr::unnest_wider(broadcasts, names_sep = "_")
+  media <- janitor::clean_names(media)
+  cbbd_save(
+    media,
+    paste0("media_", query_season),
+    "media"
+  )
+}
+
+# build all seasons
+if (Sys.getenv("TO_UPDATE") == "ALL") {
+  purrr::walk(2003:most_recent_season(), build_media, .progress = T)
+} else {
+  build_media()
+}
