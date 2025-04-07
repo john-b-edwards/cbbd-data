9efffc10b8e592091addf90c836fa27e1fe6d1a5
John Edwards <edwards1860@gmail.com>
2025-04-07 13:49:02 -0700

    add team stats

---
+++ src/update-team-stats.R
@@ -0,0 +1,34 @@
+source("R/utils.R")
+
+build_team_stats <- function(query_season = most_recent_season()) {
+  team_stats <- query_cbbd("stats/team/season", list(season = query_season)) |>
+    tidyr::unnest_wider(team_stats, names_sep = "_") |>
+    tidyr::unnest_wider(team_stats_fieldGoals, names_sep = "_") |>
+    tidyr::unnest_wider(team_stats_twoPointFieldGoals, names_sep = "_") |>
+    tidyr::unnest_wider(team_stats_threePointFieldGoals, names_sep = "_") |>
+    tidyr::unnest_wider(team_stats_freeThrows, names_sep = "_") |>
+    tidyr::unnest_wider(team_stats_rebounds, names_sep = "_") |>
+    tidyr::unnest_wider(team_stats_turnovers, names_sep = "_") |>
+    tidyr::unnest_wider(team_stats_fouls, names_sep = "_") |>
+    tidyr::unnest_wider(team_stats_points, names_sep = "_") |>
+    tidyr::unnest_wider(team_stats_fourFactors, names_sep = "_") |>
+    tidyr::unnest_wider(opponent_stats, names_sep = "_") |>
+    tidyr::unnest_wider(opponent_stats_fieldGoals, names_sep = "_") |>
+    tidyr::unnest_wider(opponent_stats_twoPointFieldGoals, names_sep = "_") |>
+    tidyr::unnest_wider(opponent_stats_threePointFieldGoals, names_sep = "_") |>
+    tidyr::unnest_wider(opponent_stats_freeThrows, names_sep = "_") |>
+    tidyr::unnest_wider(opponent_stats_rebounds, names_sep = "_") |>
+    tidyr::unnest_wider(opponent_stats_turnovers, names_sep = "_") |>
+    tidyr::unnest_wider(opponent_stats_fouls, names_sep = "_") |>
+    tidyr::unnest_wider(opponent_stats_points, names_sep = "_") |>
+    tidyr::unnest_wider(opponent_stats_fourFactors, names_sep = "_") |>
+    janitor::clean_names()
+  cbbd_save(team_stats, paste0("team_stats_", query_season), "team_stats")
+}
+
+# build all seasons
+if (Sys.getenv("TO_UPDATE") == "ALL") {
+  purrr::walk(2003:most_recent_season(), build_team_stats, .progress = T)
+} else {
+  build_team_stats()
+}
