ccac00744e488ac9328aeabc2443f031d8412f99
John Edwards <edwards1860@gmail.com>
2025-04-07 14:34:59 -0700

    add lines

---
+++ src/update-lines.R
@@ -0,0 +1,17 @@
+source("R/utils.R")
+
+build_lines <- function(query_season = most_recent_season()) {
+  lines <- query_games("lines", query_season, game_identifier = "game_id")
+  lines <- lines |>
+    tidyr::unnest_longer(lines) |>
+    tidyr::unnest(lines) |>
+    janitor::clean_names()
+  cbbd_save(lines, paste0("lines_", query_season), "lines")
+}
+
+# build all seasons
+if (Sys.getenv("TO_UPDATE") == "ALL") {
+  purrr::walk(2013:most_recent_season(), build_lines, .progress = T)
+} else {
+  build_lines()
+}
