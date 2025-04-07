124f3458000fa6ab2559b3e0795a59fe055e2a96
John Edwards <edwards1860@gmail.com>
2025-04-07 12:15:56 -0700

    adding updating teams

---
+++ src/update-teams.R
@@ -0,0 +1,14 @@
+source("R/utils.R")
+
+build_teams <- function(query_season = most_recent_season()) {
+  teams <- query_cbbd("teams", list(season = query_season))
+  teams$season <- query_season
+  cbbd_save(teams, paste0("teams_", query_season), "teams")
+}
+
+# build all seasons
+if (Sys.getenv("TO_UPDATE") == "ALL") {
+  purrr::walk(2003:most_recent_season(), build_teams, .progress = T)
+} else {
+  build_teams()
+}
