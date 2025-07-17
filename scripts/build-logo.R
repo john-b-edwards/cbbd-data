8179fe9c24215e9d7012bd725f1d23a2ca51f4c1
John Edwards <edwards1860@gmail.com>
2025-07-17 11:18:34 -0700

    logo comit

---
+++ scripts/build-logo.R
@@ -0,0 +1,15 @@
+library(hexSticker)
+imgurl <- "data/ref/basketball-clipart.png"
+sticker(
+  imgurl,
+  package = "cbbreadr",
+  p_size = 26,
+  s_x = 1,
+  s_y = 0.75,
+  s_width = 0.5,
+  h_fill = "#3f84e5",
+  h_color = "#b20d30",
+  p_color = "#f0e2e7",
+  p_y = 1.45,
+  filename = "data/outputs/cbbreadr_hex.png"
+)
